d11252bfe7076f01648e9e21cf56d631f12c4df0
VincentGuoW <sdevincentg@gmail.com>
2025-06-02 07:33:38 -0400

    eta test

--- BackEnd/EstimatedTime.java
+++ BackEnd/EstimatedTime.java
@@ -3,40 +3,34 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import java.net.URLEncoder;
 import javax.net.ssl.HttpsURLConnection;
 
 public class EstimatedTime {
-    public static String getETA(String token,String containerNumber) throws URISyntaxException, MalformedURLException, IOException{
-    
+    public static String getETA(String token, String containerNumber)
+            throws URISyntaxException, MalformedURLException, IOException {
 
-        String apiUrl = "https://api.cn.ca/customers/v1/eta";
+        if (containerNumber.length() > 4 + 6) {
+            containerNumber = containerNumber.substring(0, containerNumber.length() - 1);
+        }
 
-        String requestBody  ="{\n\"Id\":[\"" + containerNumber + "\"]\n}";
+        String apiUrl = "https://api.cn.ca/customers/v1/shipments/tracking?equipmentIds=" +
+                URLEncoder.encode(containerNumber, "UTF-8");
 
         URI uri = new URI(apiUrl);
         URL url = uri.toURL();
 
         HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setDoOutput(true);
+        connection.setRequestMethod("GET");
 
         connection.setRequestProperty("Authorization", "Bearer " + token);
         connection.setRequestProperty("Content-Type", "application/json");
-        connection.setRequestProperty("X-Limit", "100");
-
-         System.out.println("Sending request body: " + requestBody);
-         
-        try (OutputStream os = connection.getOutputStream()) {
-            byte[] input = requestBody.getBytes(StandardCharsets.UTF_8);
-            os.write(input, 0, input.length);
-        }
 
         int responseCode = connection.getResponseCode();
         if (responseCode == HttpsURLConnection.HTTP_OK) {
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                String line;
                 StringBuilder response = new StringBuilder();
+                String line;
                 while ((line = reader.readLine()) != null) {
                     response.append(line);
                 }
@@ -49,7 +43,7 @@ public class EstimatedTime {
                 while ((line = errorReader.readLine()) != null) {
                     errorResponse.append(line);
                 }
-                throw new IOException("Rq error: " + responseCode + "\n" + errorResponse);
+                throw new IOException("Request error: " + responseCode + "\n" + errorResponse);
             }
         }
 
