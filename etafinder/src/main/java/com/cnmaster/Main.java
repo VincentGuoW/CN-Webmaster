09c3a4bf2fbede352103b286d9614f3b1af4ca7c
VincentGuoW <sdevincentg@gmail.com>
2025-06-12 09:13:30 -0400

    test

--- etafinder/src/main/java/com/cnmaster/Main.java
+++ etafinder/src/main/java/com/cnmaster/Main.java
@@ -1,28 +1,25 @@
 package com.cnmaster;
 
-import java.util.Map;
-
-import com.cnmaster.BackEnd.EstimatedTime;
-import com.cnmaster.BackEnd.EtaToMap;
-import com.cnmaster.BackEnd.MapToString;
-import com.cnmaster.BackEnd.OAuthTokenFetcher;
 import com.cnmaster.FrontEnd.HomePage;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        //System.out.println(); //
-        HomePage.Mainpage();
-        
-        //String containerNumber = "EGHU9298856";
-        //String token = OAuthTokenFetcher.getAccessToken();
-        //String etaString = EstimatedTime.getETA(token, containerNumber);
-//
-        //System.out.println(etaString);
-//
-        //Map<String,Object> etaMap = EtaToMap.parseJsonToMap(etaString);
-        //MapToString.mapToString(etaMap);
-         //*/
-    }//
+        public static void main(String[] args) throws Exception {
+                // System.out.println(); //
+                HomePage.Mainpage();
 
-   
+                //String containerNumber = "TRHU4063270";
+                //String token = OAuthTokenFetcher.getAccessToken();
+                //String etaString = EstimatedTime.getETA(token, containerNumber);
+                //
+                //System.out.println(etaString);
+                //
+                //Map<String,Object> etaMap = EtaToMap.parseJsonToMap(etaString);
+                //MapToString.mapToString(etaMap);
+                ////*/
+                /// 
+                /// 
+                /// EGHU8281674
+
+        }//
+
 }
